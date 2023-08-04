e809c1fce51e6fe2cc353ff97eca90b0fb71ed9a
Anish <abcat7479@gmail.com>
2023-08-04 07:47:57 +0530

    Intial commit

---
+++ public/script.js
@@ -0,0 +1,8 @@
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskList = document.getElementById('taskList');
+    const newTask = document.createElement('li');
+    newTask.textContent = taskInput.value;
+    taskList.appendChild(newTask);
+    taskInput.value = '';
+}
